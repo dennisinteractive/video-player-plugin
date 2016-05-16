b616f6f6d3f9aa240049ffb8be2368103b90ec83
Matt Coleman <hello@matt-coleman.co.uk>
2016-05-16 16:18:57 +0100

    Added test to find Youtube api on the page, if not found it adds it and fires a callback which then initialises a Youtube iframe with the video ID

--- src/player.js
+++ src/player.js
@@ -12,6 +12,37 @@
       this.options = extendDefaults(defaults, arguments[0]);
     }
   };
+
+  Player.prototype.init = function() {
+
+    var this_ = this;
+    var callback = function() {
+
+      this_.player = new YT.Player(this_.options.playerId, {
+        videoId: this_.options.videoId,
+        }
+      });
+
+    };
+
+    // Test if the youtube api is loaded
+    if ("YT" in window) {
+      callback();
+    } else {
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      tag.onload = function() {
+        YT.ready(callback);
+      };
+    }
+
+  };
+  Player.prototype._onReady = function() {
+  };
   // Private Methods
 
   // Utility method to extend defaults with user options
