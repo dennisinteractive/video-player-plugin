6327409d28559e2a15dc172a0eaf3b0c7f8b3552
Matt Coleman <hello@matt-coleman.co.uk>
2016-05-16 16:15:13 +0100

    Added our defaults and extendDefaults function to add them to our plugin options.

--- src/player.js
+++ src/player.js
@@ -1,4 +1,28 @@
 (function() {
 
   this.Player = function() {
+    var defaults = {
+        width: 600,
+        height: 400,
+        playerId: 'video-player',
+        videoId: null,
+    };
+
+    if (arguments[0] && typeof arguments[0] === 'object') {
+      this.options = extendDefaults(defaults, arguments[0]);
+    }
+  };
+  // Private Methods
+
+  // Utility method to extend defaults with user options
+  function extendDefaults( source, properties ) {
+    var property;
+    for ( property in properties ) {
+      if ( properties.hasOwnProperty(property) ) {
+        source[property] = properties[property];
+      }
+    }
+    return source;
+  }
+
 })();
