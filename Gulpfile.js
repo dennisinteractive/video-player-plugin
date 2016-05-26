f2d79e2b565ba6af241509b2bc43dbb9c0b40946
Matt Coleman <hello@matt-coleman.co.uk>
2016-05-26 12:07:04 +0100

    Added JSlinting to project to check for mistakes

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
- 
+var jshint = require('gulp-jshint');
+var gulp   = require('gulp');
+
 gulp.task('compress', function() {
   return gulp.src('src/*.js')
     .pipe(uglify())
@@ -12,3 +14,10 @@ gulp.task('compress', function() {
     }))
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('lint', function() {
+  return gulp.src('src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
