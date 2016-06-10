8aee4c462b5351622d3ff9b5b93a31a096c98b8c
Matt Coleman <hello@matt-coleman.co.uk>
2016-06-10 12:31:32 +0100

    Updated Gulpfile.js to include little config file for directories and files. Also reordered tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,22 +6,35 @@ var rename = require("gulp-rename");
 var jshint = require('gulp-jshint');
 var gulp   = require('gulp');
 
+var config = {
+  dir: {
+    src: 'src',
+    dist: 'dist',
+  },
+  js: {
+    glob: '*.js'
+  }
+};
+
+// Check our code for errors
+gulp.task('lint', function() {
+  return gulp.src( config.dir.src + '/' + config.js.glob )
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Copy our code to dist folder
+gulp.task('copy', function() {
+  return gulp.src( config.dir.src + '/player.js')
+    .pipe(gulp.dest('dist'));
+});
+
+// Compress (and copy) our code
 gulp.task('compress', ['copy'], function() {
-  return gulp.src('src/*.js')
+  return gulp.src( config.dir.src + '/' + config.js.glob )
     .pipe(uglify())
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(gulp.dest('dist'));
-});
-
-gulp.task('lint', function() {
-  return gulp.src('src/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('copy', function() {
-   return gulp.src('src/player.js')
-   .pipe(gulp.dest('dist'));
 });
